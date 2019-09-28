f206abe5936265dd0020666b064b30d05da4c189
mah47 <mah47@students.calvin.edu>
2019-09-27 20:20:44 -0400

    lab1

---
+++ Lab01/app/src/main/java/edu/calvin/cs262/mah47/MainActivity.java
@@ -0,0 +1,17 @@
+package edu.calvin.cs262.mah47;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Log.d("MainActivity", "Hello World");
+        }
+
+}
