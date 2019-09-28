e6a12732e28fd4dfc5aa76c0b40657096c7e9247
mah47 <mah47@students.calvin.edu>
2019-09-27 20:29:04 -0400

    lab2

---
+++ Lab02/app/src/main/java/edu/calvin/cs262/mah47/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.calvin.cs262.mah47;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    /* private class */
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    /* shows hello toast notification */
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    /* increments number on display */
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
